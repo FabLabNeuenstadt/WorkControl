97bdbb0a805d11bd09e6e4df69dba5955335eb78
GityUpNow <kim-cheng@gmx.de>
2018-07-12 19:51:14 +0200

    Added extra zeros for 1-digit-numbers

--- conversion.cpp
+++ conversion.cpp
@@ -19,7 +19,15 @@ String longToTime(unsigned long convertTime) {
   convertTime /= 60;
   unsigned long hours = convertTime / 60;
   unsigned long minutes = convertTime % 60;
-  String result = String(hours) + ":" + String(minutes);
+  String hoursS = String(hours);
+  String minutesS = String(minutes);
+  if (hoursS.length() == 1)
+    hoursS = "0" + hoursS;
+
+  if (minutesS.length() == 1) 
+    minutesS = "0" + minutesS;
+    
+  String result =  hoursS + ":" + minutesS;
 
   return result;
 }
